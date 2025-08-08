8a8a06b8b350b271d168c0551270d600daff93fd
Vinícius Stolber Rocco <143605605+viniciusstolber@users.noreply.github.com>
2025-08-08 08:16:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const checkbox = document.querySelectorAll('.checkbox');
 const campoSenha = document.getElementById('campoSenha');
 const tamanhoSenhaInput = document.getElementById('tamanhoSenha');
+const forcaSenha = document.querySelector('.forca');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
@@ -21,4 +22,16 @@ let numeroAleatorio = Math.floor(Math.random() * alfabeto.length);
 senha += alfabeto[numeroAleatorio];
 }
 campoSenha.value = senha;
+classificaSenha(tamanhoSenha);
+}
+
+function classificaSenha(tamanhoSenha) {
+forcaSenha.classList.remove('fraca', 'media', 'forte');
+if (tamanhoSenha > 11) {
+forcaSenha.classList.add('forte');
+} else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+forcaSenha.classList.add('media');
+} else if (tamanhoSenha <= 5) {
+forcaSenha.classList.add('fraca');
+}
 }
